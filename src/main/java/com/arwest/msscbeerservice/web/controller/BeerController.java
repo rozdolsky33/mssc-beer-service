c71f87d02fbb7272e3666e61cf12ded602eacec5
volodymyrrozdolsky <rozdolskyvolodymyr@gmail.com>
2020-06-08 00:19:09 -0400

    adding proper static imports for restDocs

--- src/main/java/com/arwest/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/com/arwest/msscbeerservice/web/controller/BeerController.java
@@ -1,7 +1,10 @@
 package com.arwest.msscbeerservice.web.controller;
 
+import com.arwest.msscbeerservice.repositories.BeerRepository;
+import com.arwest.msscbeerservice.web.mappers.BeerMapper;
 import com.arwest.msscbeerservice.web.model.BeerDto;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -9,23 +12,35 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
 
+    private final BeerMapper beerMapper;
+    private final BeerRepository beerRepository;
+
     @GetMapping("/{beerId}")
     public ResponseEntity<BeerDto>getBeerById(@PathVariable("beerId")UUID beerId){
-        //TODO: implement
-        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+        return new ResponseEntity<>(beerMapper.BeerToBeerDto(beerRepository.findById(beerId).get()), HttpStatus.OK);
     }
+
     @PostMapping
     public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto){
-        //TODO: implement
+     beerRepository.save(beerMapper.BeerDtoToBeer(beerDto));
         return new ResponseEntity(HttpStatus.CREATED);
+
     }
     @PutMapping("{beerId}")
     public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Validated @RequestBody BeerDto beerDto ){
-        //TODO: implement
+        beerRepository.findById(beerId).ifPresent(beer -> {
+            beer.setBeerName(beerDto.getBeerName());
+            beer.setBeerStyle(beerDto.getBeerStyle().name());
+            beer.setPrice(beerDto.getPrice());
+            beer.setUpc(beerDto.getUps());
+
+            beerRepository.save(beer);
+        });
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
--- src/test/java/com/arwest/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/arwest/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,22 +1,38 @@
 package com.arwest.msscbeerservice.web.controller;
 
+import com.arwest.msscbeerservice.domain.Beer;
+import com.arwest.msscbeerservice.repositories.BeerRepository;
 import com.arwest.msscbeerservice.web.model.BeerDto;
 import com.arwest.msscbeerservice.web.model.BeerStyleEnum;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 import java.util.UUID;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "com.arwest.msscbeerservice.web.mappers")
 class BeerControllerTest {
 
     @Autowired
@@ -25,11 +41,18 @@ class BeerControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @MockBean
+    BeerRepository beerRepository;
+
     @Test
     void getBeerById() throws Exception {
+        given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-        .andExpect(status().isOk());
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(document("v1/beer", pathParameters(
+                        parameterWithName("beerId").description("UUID of desired beer to get.")
+                )));
     }
 
     @Test
